d6349b2063190d6e9ae164217c4b24e785a3143e
aviralshukla-06 <aviralshukla733@gmail.com>
2025-04-26 18:13:30 +0530

    Tried greetings function in TS

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 "use strict";
 let a = 6;
 console.log(a);
+let user = "";
+function greetings(user) {
+    console.log(`Hello ${user}`);
+}
+greetings("Aviral");
--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
 let a: number = 6
 
 console.log(a);
+
+let user: string = "";
+
+function greetings(user: string) {
+    console.log(`Hello ${user}`);
+}
+
+greetings("Aviral")
+
+
+
+
